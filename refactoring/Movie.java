19f6b225bce4689d91ac20c7cf9a07f4fb57caf2
nancyzxy <11912039@mail.sustech.edu.cn>
2021-12-17 14:35:05 +0800

    v4

--- refactoring/Movie.java
+++ refactoring/Movie.java
@@ -4,39 +4,21 @@ public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
-    String _title;
-    int _priceCode;
+    private String _title;
+    private Price _price;
 
-    public Movie(String title, int priceCode) {
+    public Movie(String title, int price) {
         this._title=title;
-        this._priceCode = priceCode;
+        set_priceCode(price);
     }
     public double getCharge(int daysRented) {
-        double thisAmount= 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2) {
-                    thisAmount += (daysRented - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (daysRented > 3) {
-                    thisAmount += (daysRented - 3) * 1.5;
-                }
-                break;
-        }
-        return thisAmount;
+        return _price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
         int frequentRenterPoints = 1;
         // add bonus for a two day new release rental
-        if ((getPriceCode() == Movie.NEW_RELEASE)
+        if ((getPriceCode().getPriceCode() == Movie.NEW_RELEASE)
                 && daysRented > 1) frequentRenterPoints++;
         return frequentRenterPoints;
     }
@@ -46,7 +28,28 @@ public class Movie {
         return _title;
     }
 
-    public int getPriceCode() {
-        return _priceCode;
+    public void set_title(String _title) {
+        this._title = _title;
+    }
+
+    public void set_priceCode(int arg) {
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
+    }
+
+    public Price getPriceCode() {
+        return _price;
     }
 }
+
---
+++ refactoring/Price.java
@@ -0,0 +1,11 @@
+package refactoring;
+
+public class Price {
+
+    public int getPriceCode() {
+        return -1;
+    }
+    public double getCharge(int daysRented){
+        return 0;
+    }
+}
